84e6119b5b54bab6256b631bc35e46e0bf61ced5
YulinDu <79042466+YulinDu@users.noreply.github.com>
2022-11-19 05:27:44 +1100

    Update bleep.hpp

--- bleep.hpp
+++ bleep.hpp
@@ -5,7 +5,10 @@ void bleep_the_text(string &input,string const keyword){
     for(int i=0;i<(input.size()-keyword.size()+1);i++){
         bool is_keyword=true;
         for(int j=0;j<keyword.size();j++){
-            if(keyword[j]!=input[i+j]) is_keyword=false;
+            if(keyword[j]!=input[i+j]){
+                is_keyword=false;
+                break;
+            } 
         }
         if(is_keyword){
             for(int j=0;j<keyword.size();j++){
